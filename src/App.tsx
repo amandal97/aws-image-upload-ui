fe49c8f4e1533f478229b40f1405761cd2da9780
amandal97 <mandaline97@gmail.com>
2022-01-03 00:12:15 +0530

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,84 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import {useDropzone} from 'react-dropzone'
+
+type UserProfilesType = {
+  userProfileId: string
+  userProfileImageLink: string
+  username: string
+}
+
+const UserProfiles = () => {
+
+  const [userProfiles, setUserProfiles] = React.useState<UserProfilesType[] | []>([]);
+
+  const fetchUserProfiles = async () => {
+    const response = await axios.get("http://localhost:8080/api/v1/user-profile")
+    
+    console.log(response)
+    
+    if (response?.data) setUserProfiles(response.data);
+    
+  }
+  
+  React.useEffect(() => {
+    fetchUserProfiles();
+  }, [])
+  
+  return (
+    <>
+
+      {userProfiles.map(userProfile => (
+        <div key={userProfile.userProfileId}>
+          <br />
+          <br />
+          <h1>{userProfile.username}</h1>
+          <p>{userProfile.userProfileId}</p>
+          <Dropzone { ...userProfile }/>
+          <br />
+        </div>
+      ))}
+    </>
+  )
+}
+
+const Dropzone = ({ userProfileId }: UserProfilesType) =>  {
+  const onDrop = React.useCallback(acceptedFiles => {
+    const file = acceptedFiles[0];
+    console.log("dropped file", file)
+    const formData = new FormData()
+    formData.append("file", file)
+    axios.post(`http://localhost:8080/api/v1/user-profile/${userProfileId}/image/upload`, 
+    formData, 
+    {
+      headers: {
+        "Content-Type": "multipart/form-data"
+      }
+    }).then(res => console.log("successful", res))
+      .catch(err => console.error("error", err))
+  }, [])
+  
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+          <p>Drop the profile image here ...</p> :
+          <p>Drag 'n' drop profile image here, or click to select profile image</p>
+      }
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+        <UserProfiles />
     </div>
+    
   );
 }
 
